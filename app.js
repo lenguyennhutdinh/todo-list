b2b0796248d346048da3d51c62185542d6f9d318
kay <lenguyennhutdinh>
2023-03-26 16:13:09 +0700

    add item

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express"
 import bodyParser from "body-parser"
 import date from "./date.js"
 import mongoose from "mongoose"
-const {Schema} = mongoose
+const { Schema } = mongoose
 
 const app = express()
 // const items = ["Buy food", "Cook food", "Eat food"]
@@ -11,36 +11,40 @@ const app = express()
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
-mongoose.connect("mongodb://127.0.0.1:27017/todo-listDB", { useNewUrlParser: true })
+mongoose
+	.connect("mongodb://127.0.0.1:27017/todo-listDB", { useNewUrlParser: true })
 	.then(() => console.log("Connected to MongoDB"))
-	.catch(err => console.log(err))
+	.catch((err) => console.log(err))
 
 const itemsSchema = {
-	name: String
+	name: String,
 }
 
-const Item = mongoose.model('Item', itemsSchema)
+const Item = mongoose.model("Item", itemsSchema)
 const item1 = new Item({
-	name: "Item 1"
+	name: "Item 1",
 })
 
 const item2 = new Item({
-	name: "Item 2"
+	name: "Item 2",
 })
 
 const item3 = new Item({
-	name: "Item 3"
+	name: "Item 3",
 })
 
-const defaultIte = [item1, item2, item3]
+const defaultItem = [item1, item2, item3]
 
-Item.insertMany(defaultIte)
-	.then(() => console.log("Items inserted successfully"))
-	.catch(err => console.log(err))
-
-app.get("/", function (req, res) {
+app.get("/", async function (req, res) {
 	const day = date.getDate()
-	res.render("list", { listTitle: day, newItems: items })
+	Item.find().then((items) => {
+		if (items.length === 0) {
+			Item.insertMany(defaultItem)
+				.then(() => console.log("Items inserted successfully"))
+				.catch(err => console.log(err))
+		}
+		res.render("list", { listTitle: day, newItems: items })
+	})
 })
 
 app.get("/work", function (req, res) {
@@ -53,12 +57,12 @@ app.get("/about", function (req, res) {
 
 app.post("/", function (req, res) {
 	const item = req.body.newItem
-	console.log(req.body)
 	if (req.body.list === "Work") {
-		workItems.push(item)
+		// workItems.push(item)
 		res.redirect("/work")
 	} else {
-		items.push(item)
+		// items.push(item)
+		Item.insertMany({ name: item })
 		res.redirect("/")
 	}
 })
