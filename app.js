3976b18d6579e76452d2d6bdcbcb16979d53c44d
kay <lenguyennhutdinh>
2023-03-26 15:13:25 +0700

    Connect DB

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 import express from "express"
 import bodyParser from "body-parser"
 import date from "./date.js"
+import mongoose from "mongoose"
+const {Schema} = mongoose
 
 const app = express()
-const items = ["Buy food", "Cook food", "Eat food"]
-const workItems = []
+// const items = ["Buy food", "Cook food", "Eat food"]
+// const workItems = []
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }))
+mongoose.connect("mongodb://127.0.0.1:27017/todo-listDB", { useNewUrlParser: true })
+	.then(() => console.log("Connected to MongoDB"))
+	.catch(err => console.log(err))
+
+const itemsSchema = {
+	name: String
+}
+
+const Item = mongoose.model('Item', itemsSchema)
+const item1 = new Item({
+	name: "Item 1"
+})
+
+const item2 = new Item({
+	name: "Item 2"
+})
+
+const item3 = new Item({
+	name: "Item 3"
+})
+
+const defaultIte = [item1, item2, item3]
+
+Item.insertMany(defaultIte)
+	.then(() => console.log("Items inserted successfully"))
+	.catch(err => console.log(err))
 
 app.get("/", function (req, res) {
 	const day = date.getDate()
